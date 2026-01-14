97b5482c70e25ca7390650a1756d20038b31f6d7
Kamlesh Pandey <kamleshpandey25k@gmail.com>
2026-01-14 23:41:05 +0530

    added SecondLargestOptimal function

--- SecondLargestElement.py
+++ SecondLargestElement.py
@@ -1,3 +1,15 @@
+def secondLargestOptimal(num):
+    first=float('-inf')
+    s_largest=float('-inf')
+    n=len(num)
+    for i in range(0,n):
+        if num[i] > first:
+            s_largest=first
+            first=num[i]
+        elif s_largest < num[i] and num[i] != first :
+             s_largest=num[i]
+    return s_largest
+
 def secondLargestElement(num):
     first=float('-inf')
     second=float('-inf')
@@ -10,4 +22,4 @@ def secondLargestElement(num):
           second = num[i]
     return second
 num=[10,30,50,22,88,55,44]
-print(secondLargestElement(num))
+print(secondLargestOptimal(num))
