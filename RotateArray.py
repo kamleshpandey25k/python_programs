49c23708b766988c46764009f302734ff9cf8c08
Kamlesh Pandey <kamleshpandey25k@gmail.com>
2026-01-16 02:03:25 +0530

    resolved leetcode issue

--- RotateArray.py
+++ RotateArray.py
@@ -6,9 +6,10 @@ def swap(num , i,j):
 def rotateArray(num , k):
    n=len(num)
    k=k % n
+   swap(num,0, n-1)
    swap(num,0,k-1)
    swap(num , k,n-1)
-   swap(num,0, n-1)
+ 
 num=[10,20,30,40,50,60,70]
 rotateArray(num,2)
 print(num)
