8195000b771a4eaad78c89b9c34a60d1c61a5295
Kamlesh Pandey <kamleshpandey25k@gmail.com>
2026-01-29 02:18:23 +0530

    Add ListCamparision program

---
+++ ListCamparision.py
@@ -0,0 +1,17 @@
+# ==
+l1=[10,40,30,20]
+l2=[20,40,30,20,]
+print(l1==l2)
+print(l1<=l2)
+l3=l1*3
+#print(l3)
+l1.append(90)
+print(l1)
+print(l1.pop())
+l1.insert(100,100)
+print(l1)
+l1.remove(100)
+print(l1)
+l1.reverse()
+print(l1)
+print(l1.index(30))
