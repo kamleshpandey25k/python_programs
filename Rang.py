a130047f055a8ea1d9ff69444871affc92d93ad7
Kamlesh Pandey <kamleshpandey25k@gmail.com>
2026-01-31 21:11:02 +0530

    Add Range data type program

---
+++ Rang.py
@@ -0,0 +1,12 @@
+for i in range(10,0,-2):
+    print(i)
+for i in range(0,10,2):
+    print(i)
+r=range(1,21,2)
+for i in r:
+    print(i)
+for i in range(10):
+    print(i)
+n = int(input("Enter the number"))
+for i in range(n+1):
+    print(i**2)
